2c72a41c87a8d73483005bf769f72d07276f701d
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-08-08 17:49:51 +0300

    feat: add body close

--- internal/pusher/pusher.go
+++ internal/pusher/pusher.go
@@ -2,8 +2,10 @@ package pusher
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"screamer/internal/collector"
 	"screamer/internal/config"
 )
@@ -22,6 +24,12 @@ func request(method, kind, name, value string) {
 	c := config.GetConfig()
 	url := fmt.Sprintf("%v/%v/%v/%v/%v", c.ServerUrl, method, kind, name, value)
 	r, err := http.Post(url, "text/plain", nil)
+	if err == nil {
+		_, _ = io.Copy(os.Stdout, r.Body)
+		defer func(Body io.ReadCloser) {
+			_ = Body.Close()
+		}(r.Body)
+	}
 	if c.AgentLogEnable {
 		if err != nil {
 			log.Println("Request error ", err.Error())
