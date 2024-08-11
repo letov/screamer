e054a0049aa0d2b8f1d0598e914442ae5a19b3fe
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-08-11 15:51:20 +0300

    feat: add cmd line support

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"screamer/internal/args"
 	"screamer/internal/config"
+	"screamer/internal/grab"
+	"screamer/internal/storage"
 )
 
 func init() {
 	args.InitServer()
 	config.InitServer()
-	//storage.Init()
-	//grab.Init()
+	storage.Init()
+	grab.Init()
 }
 
 func main() {
