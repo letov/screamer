7328c0a395214a2705c3d438d80f4a2435d47955
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-08-24 18:38:01 +0300

    feat: added handlers

---
+++ internal/grab/handlers/value_old.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"screamer/internal/metric"
+	"screamer/internal/storage"
+	"screamer/internal/storage/repos/kinds"
+)
+
+func ValueMetricOld(res http.ResponseWriter, req *http.Request) {
+	label := chi.URLParam(req, "label")
+	name := chi.URLParam(req, "name")
+
+	k, err := metric.LabelToKind(label)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	s := storage.GetStorage()
+	if s == nil {
+		http.Error(res, ErrNoStorage.Error(), http.StatusBadRequest)
+		return
+	}
+
+	v, err := s.GetLastAsString(k, name)
+	if err != nil {
+		if err == kinds.ErrEmptyMetric {
+			http.Error(res, err.Error(), http.StatusNotFound)
+		} else {
+			http.Error(res, err.Error(), http.StatusBadRequest)
+		}
+		return
+	}
+
+	_, err = res.Write([]byte(v))
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
