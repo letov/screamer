0631432c2b6da6c9fb02307e83b239ee2d2ffe39
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-09-01 20:40:05 +0300

    feat: added signals

--- internal/agent/services/shutdown-service.go
+++ internal/agent/services/shutdown-service.go
@@ -12,7 +12,7 @@ type ShutdownService struct {
 func (ss *ShutdownService) Run() {
 	sigs := make(chan os.Signal, 1)
 
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	done := make(chan bool, 1)
 
 	go func() {
