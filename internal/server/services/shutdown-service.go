08bc89d8ff769b07794ac6474dc7020076d646c9
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-09-01 18:59:22 +0300

    feat: added backup

--- internal/server/services/shutdown-service.go
+++ internal/server/services/shutdown-service.go
@@ -11,13 +11,41 @@ type ShutdownService struct {
 }
 
 func (ss *ShutdownService) Run() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal_chan := make(chan os.Signal, 1)
+	signal.Notify(signal_chan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	exit_chan := make(chan int)
 	go func() {
-		<-c
-		ss.backupService.Save()
-		os.Exit(1)
+		for {
+			s := <-signal_chan
+			switch s {
+			case syscall.SIGHUP:
+				ss.backupService.Save()
+
+			case syscall.SIGINT:
+				ss.backupService.Save()
+
+			case syscall.SIGTERM:
+				ss.backupService.Save()
+				exit_chan <- 0
+
+			case syscall.SIGQUIT:
+				ss.backupService.Save()
+				exit_chan <- 0
+
+			default:
+				ss.backupService.Save()
+				exit_chan <- 1
+			}
+		}
 	}()
+
+	code := <-exit_chan
+	os.Exit(code)
 }
 
 func NewShutdownService(bs *BackupService) *ShutdownService {
