f7989e310581611b5dc18fcc8907926f08c35bbf
Ruslan Karymov <ruslan-leto@yandex-team.ru>
2024-12-12 20:43:36 +0300

    added build info

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"screamer/internal/agent/di"
+	"screamer/internal/common/build"
 	event_loop "screamer/internal/common/eventloop"
 	"screamer/internal/common/prof"
 
 	"go.uber.org/fx"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	build.ShowBuildParams(buildVersion, buildDate, buildCommit)
 	fx.New(
 		di.InjectApp(),
 		fx.Invoke(func(*event_loop.EventLoop, *prof.Server) {}),
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"net/http"
+	"screamer/internal/common/build"
 	event_loop "screamer/internal/common/eventloop"
 	"screamer/internal/server/di"
 
 	"go.uber.org/fx"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	build.ShowBuildParams(buildVersion, buildDate, buildCommit)
 	fx.New(
 		di.InjectApp(),
 		fx.Invoke(func(*event_loop.EventLoop, *http.Server) {}),
